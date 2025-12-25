39dfd18ceaf299c383e323e9cecd7db86831a668
mikeryu98 <mikeryu98@gmail.com>
2025-12-25 14:38:49 +0900

    v1.2 Device 조회시, 401 인터셉터 구현 (use refreshToken)

--- accessories/BaseAccessory.js
+++ accessories/BaseAccessory.js
@@ -11,6 +11,30 @@ class BaseAccessory {
             baseURL: `https://api.smartthings.com/v1/devices/${this.deviceId}`,
             headers: { 'Authorization': `Bearer ${this.platform.accessToken}` }
         });
+
+        this.client.interceptors.response.use(
+            (response) => response,
+            async (error) => {
+                const originalRequest = error.config;
+
+                if (error.response && error.response.status === 401 && !originalRequest._retry) {
+                    originalRequest._retry = true;
+                    this.log.warn(`[${this.name}] 토큰 만료 감지. 갱신을 시도합니다...`);
+
+                    try {
+                        const newToken = await this.platform.refreshAccessToken();
+
+                        originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+                        this.client.defaults.headers['Authorization'] = `Bearer ${newToken}`;
+
+                        return this.client(originalRequest);
+                    } catch (refreshError) {
+                        this.log.error('토큰 갱신 실패. 수동 인증이 필요할 수 있습니다.');
+                    }
+                }
+                return Promise.reject(error);
+            }
+        );
     }
 
     async executeCommand(capability, command, args = []) {
